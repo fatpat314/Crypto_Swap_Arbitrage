cf00f5e011d3864792dfa03c1560fb128c4d5997
Patrick <w.patrick.kelly@gmail.com>
2021-07-15 17:30:24 -0700

    added list of crypto to test recurrent process

--- auto.py
+++ auto.py
@@ -12,8 +12,6 @@ chrome_options.add_argument("--headless")
 ETH = 1
 target = 5
 
-token1_list = ['USDT', 'BTC', 'ETH']
-token2_list = ['DOGE', 'DAI', 'BTCH']
 
 
 token1 = 'USDT'
@@ -29,7 +27,7 @@ kyber = ''
 
 prices = []
 
-async def sushi_swap():
+async def sushi_swap(token1, token2):
     # web = webdriver.Chrome(options=chrome_options)
     # web = webdriver.Chrome()
     # web.set_window_position(-10000,0)
@@ -46,7 +44,7 @@ async def sushi_swap():
     await asyncio.sleep(2)
 
     # coin_select = web.find_element_by_class_name('css-8mokm4')
-    coin_select = sushi_web.find_element_by_xpath('//*[@id="token-item-0xdAC17F958D2ee523a2206206994597C13D831ec7"]')
+    coin_select = sushi_web.find_element_by_xpath('/html/body/reach-portal[1]/div[3]/div/div/div/div/div/div/div[4]/div[1]/div/div/div[1]')
     # time.sleep(2)
     # await asyncio.sleep(5)
     coin_select.click()
@@ -62,9 +60,15 @@ async def sushi_swap():
     # time.sleep(2)
     await asyncio.sleep(2)
     #
-    # coin_select = web.find_element_by_class_name('css-8mokm4')
-    coin_select = sushi_web.find_element_by_xpath('//*[@id="token-item-ETHER"]')
+    # coin_select = sushi_web.find_element_by_class_name("w-full flex p-0 Row__RowBetween-sc-18omu2-0 styleds__MenuItem-sc-spkdn1-7 dOvlka hsCgeH hover:bg-dark-800 rounded")
+    coin_select = sushi_web.find_element_by_xpath('/html/body/reach-portal[1]/div[3]/div/div/div/div/div/div/div[4]/div[1]/div/div/div[1]/div[1]')
+    # coin_select = sushi_web.find_element_by_css_selector('#token-item-ETHER')
+
+
+    
     coin_select.click()
+
+    await asyncio.sleep(2)
 
     input = sushi_web.find_element_by_xpath('//*[@id="swap-currency-input"]/div/div[2]/input')
     input.send_keys(1)
@@ -77,10 +81,10 @@ async def sushi_swap():
     global sushi
     sushi = sushi_price
     prices.append(sushi)
-    # sushi_web.quit()
+    sushi_web.quit()
     return sushi_price
 
-async def uni_swap():
+async def uni_swap(token1, token2):
     # web = webdriver.Chrome()
     # web = webdriver.Chrome(options=chrome_options)
     uni_web.get('https://app.uniswap.org/#/swap')
@@ -114,7 +118,8 @@ async def uni_swap():
     # time.sleep(2)
     await asyncio.sleep(2)
 
-    coin_select = uni_web.find_element_by_class_name('css-8mokm4')
+    # coin_select = uni_web.find_element_by_class_name('css-8mokm4')
+    coin_select = uni_web.find_element_by_xpath('/html/body/reach-portal[1]/div[3]/div/div/div/div/div[3]/div[1]/div/div/div[1]')
     coin_select.click()
     # time.sleep(2)
     await asyncio.sleep(2)
@@ -127,12 +132,12 @@ async def uni_swap():
     print('Uni Price: ', uni_price)
     uni = uni_price
     prices.append(uni_price)
-    # uni_web.quit()
+    uni_web.quit()
     return uni_price
 
 
 
-async def kyber_swap():
+async def kyber_swap(token1, token2):
     # web = webdriver.Chrome()
     # web = webdriver.Chrome(options=chrome_options)
     kyber_web.get('https://www.kyberswap.com/swap/' + token1 + '-' + token2)
@@ -153,12 +158,12 @@ async def kyber_swap():
     await asyncio.sleep(5)
 
     read_coin_price = kyber_web.find_element_by_xpath('//*[@id="content"]/div[1]/div/div[1]/div/div[1]/div/div[3]/div[3]/div/div/div/span').text
-    kyber_price = read_coin_price[9:18] #.get_property('value')
+    kyber_price = read_coin_price[9:16] #.get_property('value')
     print('Kyber Price: ', kyber_price)
     global kyber
     kyber = kyber_price
     prices.append(kyber)
-    # kyber_web.quit()
+    kyber_web.quit()
     return kyber_price
 
 def do_maths():
@@ -166,39 +171,43 @@ def do_maths():
     sushi = prices[1]
     uni = prices[2]
 
-    sushi = float(sushi)
-    uni = float(uni)
-    kyber = float(kyber)
+    if kyber == '' or sushi == '' or uni == '':
+        pass
+    else:
+        sushi = float(sushi)
+        uni = float(uni)
+        kyber = float(kyber)
 
-    change_percent_sushi_uni = (abs(sushi-uni)/uni)
-    change_percent_uni_kyber = (abs(uni-kyber)/kyber)
-    change_percent_kyber_sushi = (abs(kyber-sushi)/sushi)
+        
+        change_percent_sushi_uni = (abs(sushi-uni)/uni)
+        change_percent_uni_kyber = (abs(uni-kyber)/kyber)
+        change_percent_kyber_sushi = (abs(kyber-sushi)/sushi)
 
-    if change_percent_sushi_uni > target:
-        print(token1 + "/" + token2 + " sushi/uni", change_percent_sushi_uni )
+        if change_percent_sushi_uni > target:
+            print(token1 + "/" + token2 + " sushi/uni", change_percent_sushi_uni )
 
-        # sendmail("Sushi/Uni percentage differance is " + str(change_percent_sushi_uni))
+            # sendmail("Sushi/Uni percentage differance is " + str(change_percent_sushi_uni))
 
-    if change_percent_uni_kyber > target:
-        print(token1 + "/" + token2 + " uni/kyber", change_percent_uni_kyber)
+        if change_percent_uni_kyber > target:
+            print(token1 + "/" + token2 + " uni/kyber", change_percent_uni_kyber)
 
-        # sendmail("Uni/Kyber percentage differance is " + str(change_percent_uni_kyber))
+            # sendmail("Uni/Kyber percentage differance is " + str(change_percent_uni_kyber))
 
-    if change_percent_kyber_sushi > target:
-        print(token1 + "/" + token2 + " kyber/sushi", change_percent_kyber_sushi)
+        if change_percent_kyber_sushi > target:
+            print(token1 + "/" + token2 + " kyber/sushi", change_percent_kyber_sushi)
 
-        # sendmail("Kyber/Sushi percentage differance is " + str(change_percent_kyber_sushi))
+            # sendmail("Kyber/Sushi percentage differance is " + str(change_percent_kyber_sushi))
 
-    if change_percent_sushi_uni < target and change_percent_sushi_uni < target and change_percent_uni_kyber < target:
-        print("No change")
-        print(token1, token2)
-        print("Sushi/Uni: ", change_percent_sushi_uni)
-        print("Uni/Kyber: ", change_percent_uni_kyber)
-        print("Kyber/Sushi: ", change_percent_kyber_sushi)
-    print("----------------------------------------------------")
+        if change_percent_sushi_uni < target and change_percent_sushi_uni < target and change_percent_uni_kyber < target:
+            print("No change")
+            print(token1, token2)
+            print("Sushi/Uni: ", change_percent_sushi_uni)
+            print("Uni/Kyber: ", change_percent_uni_kyber)
+            print("Kyber/Sushi: ", change_percent_kyber_sushi)
+        print("----------------------------------------------------")
 
 
-    return change_percent_sushi_uni, change_percent_uni_kyber, change_percent_kyber_sushi
+        return change_percent_sushi_uni, change_percent_uni_kyber, change_percent_kyber_sushi
 
 # def sendmail(message):
 #     message = message
@@ -227,18 +236,18 @@ def do_maths():
     # time.sleep(5)
 async def main():
     global sushi_web
-    sushi_web = webdriver.Chrome()
+    sushi_web = webdriver.Chrome(options=chrome_options)
     global uni_web
-    uni_web = webdriver.Chrome()
+    uni_web = webdriver.Chrome(options=chrome_options)
     global kyber_web
-    kyber_web = webdriver.Chrome()
+    kyber_web = webdriver.Chrome(options=chrome_options)
 
     tasks = []
     # for i in range(10):
     # sushi_swap()
-    tasks.append(asyncio.ensure_future(sushi_swap()))
-    tasks.append(asyncio.ensure_future(uni_swap()))
-    tasks.append(asyncio.ensure_future(kyber_swap()))
+    tasks.append(asyncio.ensure_future(sushi_swap(token1, token2)))
+    tasks.append(asyncio.ensure_future(uni_swap(token1, token2)))
+    tasks.append(asyncio.ensure_future(kyber_swap(token1, token2)))
     # i+=1
     print('hi')
     await asyncio.gather(*tasks)
@@ -246,14 +255,22 @@ async def main():
     print('hi')
 
 while True:
-    # task = []
-    # task.append(asyncio.ensure_future(main()))
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    print('PRICES: ', prices)
-    do_maths()
-    prices.clear()
-    # loop.close()
+    # token1_list = ['USDT', 'BTC', 'ETH']
+    # token2_list = ['DOGE', 'DAI', 'BTCH']
+    token1_list = ['USDT', 'BTC', 'DAI']
+    token2_list = ['ETH', 'BAT', 'AMP']
+    for token1 in token1_list:
+        for token2 in token2_list:
+
+            # task = []
+            # task.append(asyncio.ensure_future(main()))
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(main())
+            print('PRICES: ', prices)
+            print('Comparing: ', token1, ' and ', token2)
+            do_maths()
+            prices.clear()
+            # loop.close()
 
 
 
